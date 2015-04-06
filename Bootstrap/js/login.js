8dfb2520a3d70549cd90ef8f5949f8a268e6040b
pingandrew@gmail.com <pingandrew@gmail.com>
2015-04-05 23:25:05 -0400

    Adding Jeffreys login changes

---
+++ Bootstrap/js/login.js
@@ -0,0 +1,29 @@
+/**
+ * Created by jeffrey on 15/4/6.
+ */
+
+$(function () {
+    "use strict";
+    $("#login").on("submit", function (e) {
+        var username_or_email = $("#login input[name=email_or_username]").val();
+        var password = $("#login input[name=password]").val();
+
+        var data = {
+            username_or_email: username_or_email,
+            password: password
+        };
+
+        $.ajax({
+            url: "http://54.172.140.235/api/v1/user/login/",
+            contentType: "application/json",
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify(data),
+            success: function (result) {
+                console.log(result);
+                alert(JSON.stringify(result));
+            }
+        });
+        return false;
+    });
+});
